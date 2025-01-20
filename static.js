3c5e89e0c10238a0661abde474fb7d68fd3fed5a
Nabeel Amir <140976732+NabeelAmir28@users.noreply.github.com>
2025-01-20 13:10:35 +0500

    Create static.js

---
+++ static.js
@@ -0,0 +1,4 @@
+// Function to change the text content dynamically
+function changeText() {
+  document.getElementById("dynamic-text").textContent = "You clicked the button! The text has changed.";
+}
